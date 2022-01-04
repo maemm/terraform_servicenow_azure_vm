cc27940f4037dbbb95ad1bbb7773eed61ef89d03
DanielHuerta <danielhuerta@DanielHuerta-MacBook-Pro.local>
2022-01-04 17:03:16 -0600

    Creating vnet and subnet

--- main.tf
+++ main.tf
@@ -1,19 +1,37 @@
+data "azurerm_resource_group" "rg" {
+	name = "RG-USWST2-RPS-TFC-POC"
+}
+
+resource "azurerm_virtual_network" "vnet" {
+  name                = "vnet"
+  address_space       = ["10.0.0.0/16"]
+  location            = data.azurerm_resource_group.rg.location
+  resource_group_name = data.azurerm_resource_group.rg.name
+}
+
+resource "azurerm_subnet" "subnet" {
+  name                 = "internal"
+  resource_group_name  = data.azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.2.0/24"]
+}
+
 resource "azurerm_network_interface" "nic" {
   name                = "nic"
-  location            = "US WEST 2"
-  resource_group_name = "RG-USWST2-RPS-TFC-POC"
+  location            = data.azurerm_resource_group.rg.location
+  resource_group_name = data.azurerm_resource_group.rg.name
 
   ip_configuration {
     name                          = "internal"
-    subnet_id                     = "SUB-USWST2-RPS-RPS1"
+    subnet_id                     = azurerm_subnet.subnet.id
     private_ip_address_allocation = "Dynamic"
   }
 }
 
 resource "azurerm_linux_virtual_machine" "vm" {
   name                = "poc-virtual-machine"
-  resource_group_name = "RG-USWST2-RPS-TFC-POC"
-  location            = "US WEST 2"
+  resource_group_name = data.azurerm_resource_group.rg.name
+  location            = data.azurerm_resource_group.rg.location
   size                = var.vm_size
   admin_username      = "adminuser"
   network_interface_ids = [
@@ -35,7 +53,7 @@ resource "azurerm_linux_virtual_machine" "vm" {
 	tags = {
 		RequestIT = "I0026089",
 		Cost_Center = "842",
-		Created_By_Email = "daniel.huerta@contractor.amat.com"
+		Created_By_Email = "techmahindra.team@contractor.amat.com"
 	}
 }
 
